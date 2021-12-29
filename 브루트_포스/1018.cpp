3647deb2316a11a5e39c82b118dc763bb5257368
yimethan <67178689+ethannotsteve@users.noreply.github.com>
2021-12-29 23:52:22 +0900

    brute force

--- 브루트_포스/1018.cpp
+++ 브루트_포스/1018.cpp
@@ -1,9 +1,67 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-    int n, m;
-    cin >> n, m;
+string board[50];
 
-    int 
+string WB[8] = {
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW"
+};
+
+string BW[8] = {
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB",
+        "BWBWBWBW",
+        "WBWBWBWB"
+};
+
+int countW(int x, int y){
+    int count=0;
+
+    for(int i=0; i<8; i++){
+        for(int j=0; j<8; j++){
+            if(board[x+i][y+j] != WB[i][j]) count++;
+        }
+    }
+    return count;
+}
+
+int countB(int x, int y){
+    int count=0;
+    for(int i=0; i<8; i++){
+        for(int j=0; j<8; j++){
+            if(board[x+i][y+j] != BW[i][j]) count++;
+        }
+    }
+    return count;
+}
+
+int main(){
+    int n, m, count=0, min=500;
+    cin >> n >> m;
+
+    for(int i=0; i<n; i++) cin >> board[i];
+
+    for(int i=0; i+8 <= n; i++){
+        for(int j=0; j+8 <= m; j++){
+            int count1 = countW(i, j);
+            int count2 = countB(i, j);
+            if(count1 < count2){
+                if(count1 < min) min = count1;
+            } else {
+                if(count2 < min) min = count2;
+            }
+        }
+    }
+    cout << min;
 }
